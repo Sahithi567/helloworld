6182f196a20535d3f6c7182b8153e8ec0863f151
Sahithi567 <96521495+Sahithi567@users.noreply.github.com>
2021-12-22 16:19:31 +0530

    Add files via upload

---
+++ fifth.py
@@ -0,0 +1,9 @@
+n=5
+for i in range(1,n+1):
+    for k in range(i,n):
+        print(" ",end='')
+    for j in range(1,i+1):
+         print(j,end='') 
+    for l in range(i-1,0,-1)
+        print(l,end='')
+    print()
